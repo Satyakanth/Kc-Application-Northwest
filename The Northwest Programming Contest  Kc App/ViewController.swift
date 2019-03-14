35b2e5b1db80d90f498a8bb111d523a2792cf18d
Student <student@nw41493.nwmissouri.edu>
2019-03-14 15:34:26 -0500

    Initial Commit

---
+++ The Northwest Programming Contest  Kc App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Northwest Programming Contest  Kc App
+//
+//  Created by Student on 3/14/19.
+//  Copyright © 2019 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
